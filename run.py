cafa26b42c367a1dc72810fe6af0c8820b1dafa3
root <root@ns361240.ip-91-121-166.eu>
2018-05-12 19:20:24 +0000

    intial push

---
+++ run.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+
+def cron():
+    while True:
+	#print "Started new job"
+        time.sleep(300)
+        os.popen("/root/.nvm/versions/node/v9.7.1/bin/node scripts/sync.js index update")
+        os.popen("/root/.nvm/versions/node/v9.7.1/bin/node scripts/peers.js")
+    
+    
+if __name__=="__main__":
+    cron()
